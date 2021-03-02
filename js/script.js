9418141e8a335b484e1e29dc68f60d0a5fdd0c63
Muriel Oliveira <53922623+MurielOliveira@users.noreply.github.com>
2021-03-01 21:35:53 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.querySelector(".sacola").addEventListener("click", ()=>
+    document.querySelector(".sacola-sidebar").classList.toggle("show-menu")
+);
+
+let time = 3000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll(".slider>img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
